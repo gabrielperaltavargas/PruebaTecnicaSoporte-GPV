bdf7b0abbcc6d8686eef6ab13709b43166df0a19
gabrielperaltavargas <gabrielperaltavargas9@gmail.com>
2024-02-26 17:23:56 -0300

    Segundo Commit

--- app/Http/Controllers/PruebaController.php
+++ app/Http/Controllers/PruebaController.php
@@ -14,16 +14,16 @@ class PruebaController extends Controller
     {
         $aplicante = [
             'puntos' => 0,
-            'nivel' => 0
-            'nombre' => 'Jhon Doe',
+            'nivel' => 1,  // Según las indicaciones de README, el aplicante debe partir de Nivel 1, además hacía falta una coma para terminar la asignación inicial de la variable  
+            'nombre' => 'Gabriel Peralta', // Nombre del aplicante 
             'aprobado' => false
         ];
-        while ($aplicante['nivel'] > 10) {
+        while ($aplicante['nivel'] < 10) { // El While debe durar hasta alcanzado el Nivel 10, empezando con 1, por eso la variable debe ser menos a 10 y llegado a 10 es cuando termina la operación. 
             $aplicante = $this->entrenar($aplicante);
         }
         $aplicante['aprobado'] = $this->evaluar($aplicante);
 
-        return view('prueba');
+        return view('prueba', compact('aplicante')); // Como aplicante es un Array ---9/97089/3513914-h
     }
 
     /**
@@ -31,9 +31,12 @@ class PruebaController extends Controller
      * @param array $aplicante
      * @reutrn array
      */
-    private function entrenar(int $aplicante)
+    private function entrenar(array $aplicante) //La variable $aplicante definida en la linea 15 no es un Int, es un Array de diferentes tipos de variables. 
     {
         $aplicante['puntos'] += 10 / $aplicante['nivel'];
+		
+		print_r($aplicante['puntos'] += 10 / $aplicante['nivel']);
+		
         if ($aplicante['puntos'] >= 100) {
             $aplicante['nivel']++;
             $aplicante['puntos'] = 0;
@@ -48,6 +51,6 @@ class PruebaController extends Controller
      */
     private function evaluar(array $aplicante)
     {
-        return $aplicante->nivel >= 20;
+        return $aplicante['nivel'] >= 10;  // El proceso dura hasta alcanzado el nivel 10
     }
 }
